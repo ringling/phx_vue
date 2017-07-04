bdfd27df2d02c7cc38d94cd025b05edd0cad0d28
Thomas Ringling <thomas.ringling@gmail.com>
2017-07-04 21:38:49 +0200

    don't start Repo

--- lib/phx_vue/application.ex
+++ lib/phx_vue/application.ex
@@ -9,7 +9,7 @@ defmodule PhxVue.Application do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(PhxVue.Repo, []),
+      # supervisor(PhxVue.Repo, []),
       # Start the endpoint when the application starts
       supervisor(PhxVue.Web.Endpoint, []),
       # Start your own worker by calling: PhxVue.Worker.start_link(arg1, arg2, arg3)
