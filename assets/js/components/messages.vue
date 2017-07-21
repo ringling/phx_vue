a346b26fd46d8885679ed6d76a8153e4dcaebf48
Thomas Ringling <thomas@ringling.info>
2017-07-21 10:42:27 +0200

    Use vuetify

--- assets/js/components/messages.vue
+++ assets/js/components/messages.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="messages">
-    <h2>Messages</h2>
-    <input type="text" v-model="message" v-on:keyup.13="sendMessage">
-    <ul v-for="message in messages">
-      <li>{{message}}</li>
-    </ul>
-  </div>
+  <v-layout row>
+    <v-flex xs12 sm6>
+      <v-card>
+        <v-toolbar class="teal" dark>
+          <v-toolbar-title>message</v-toolbar-title>
+        </v-toolbar>
+        <v-text-field 
+          v-model="message"
+          name="input-1"
+          label="Chat message"
+          id="testing" 
+          v-on:keyup.native.13="sendMessage"></v-text-field>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 sm6>
+      <v-card>
+        <v-toolbar class="teal" dark>
+          <v-toolbar-title>Chat</v-toolbar-title>
+        </v-toolbar>
+        <v-list two-line subheader>
+          <v-subheader>Messages</v-subheader>
+
+          <v-list-tile avatar v-for="message in messages">
+            <v-list-tile-content>
+              <v-list-tile-title>{{message.body}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{message.received_at}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+         
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -17,10 +43,17 @@ export default {
    //     return this.$store.state.messages
    //   }
    // },
+
+   data() {
+    return {
+      message: null
+    }
+  },
+
   methods: {
     sendMessage() {
       this.$parent.channel.push("new_msg", { body: this.message })
-      this.message = ''
+      this.message = null
     }
   }
 }
